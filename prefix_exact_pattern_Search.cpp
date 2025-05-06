d862acf9f607043a048587c405889b5c3a6d034e
Jahidul Islam <jahidulzaid@gmail.com>
2025-05-06 11:00:56 +0600

    added the build app

--- prefix_exact_pattern_Search.cpp
+++ prefix_exact_pattern_Search.cpp
@@ -84,12 +84,11 @@ bool linear_search_exact(vector<string> v, string str)
 // Fills lps[] for given patttern pat[0..M-1]
 void computeLPSArray(string pat, int M, int *lps)
 {
-    // length of the previous longest prefix suffix
     int len = 0;
 
-    lps[0] = 0; // lps[0] is always 0
+    lps[0] = 0;
 
-    // the loop calculates lps[i] for i = 1 to M-1
+    // the loop calculates lps[i] for i to M-1
     int i = 1;
     while (i < M)
     {
@@ -99,17 +98,13 @@ void computeLPSArray(string pat, int M, int *lps)
             lps[i] = len;
             i++;
         }
-        else // (pat[i] != pat[len])
+        else
         {
-            // This is tricky. Consider the example.
-            // AAACAAAA and i = 7. The idea is similar
-            // to search step.
+
             if (len != 0)
             {
                 len = lps[len - 1];
 
-                // Also, note that we do not increment
-                // i here
             }
             else // if (len == 0)
             {
@@ -126,15 +121,12 @@ int KMPSearch(string pat, string txt)
     int M = pat.size();
     int N = txt.size();
 
-    // create lps[] that will hold the longest prefix suffix
-    // values for pattern
     int lps[M];
 
-    // Preprocess the pattern (calculate lps[] array)
     computeLPSArray(pat, M, lps);
 
-    int i = 0; // index for txt[]
-    int j = 0; // index for pat[]
+    int i = 0;
+    int j = 0; 
     while (i < N)
     {
         if (pat[j] == txt[i])
@@ -145,7 +137,6 @@ int KMPSearch(string pat, string txt)
 
         if (j == M)
         {
-            // printf("Found pattern at index %d ", i - j);
             return i - j;
             j = lps[j - 1];
         }
@@ -153,8 +144,7 @@ int KMPSearch(string pat, string txt)
         // mismatch after j matches
         else if (i < N && pat[j] != txt[i])
         {
-            // Do not match lps[0..lps[j-1]] characters,
-            // they will match anyway
+
             if (j != 0)
                 j = lps[j - 1];
             else
@@ -164,7 +154,7 @@ int KMPSearch(string pat, string txt)
     return -1;
 }
 
-// GLOBAL Search
+// Universal Search
 
 vector<string> search(vector<string> v, string pat)
 {
